aabc2904987b752554606954f7d7ad11f00e09f3
Krampus138 <82593530+Grumpy63@users.noreply.github.com>
2021-05-13 18:09:13 +0200

    erste Tests

--- eclipse_shared_workspace/test_01/main.c
+++ eclipse_shared_workspace/test_01/main.c
@@ -10,7 +10,16 @@
 int main(void)
 {
 
-	printf("Hello World");
+	setbuf(stdout, NULL);
+
+
+	int a = 0;
+
+	printf("Hello World, bitte gib eine Zahl ein: ");
+
+	scanf("%d", &a);
+
+	printf("Die Zahl lautet %d.", a);
 
 	return 0;
 }
