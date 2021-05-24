04e6491b2f22daa7c7082f27eb81cbc0d69c332a
FishIsMight <david.braun-1@gmx.de>
2021-05-24 21:30:48 +0200

    Create Test-Project
    Create a test project and started toying with code to read txt files. worked so far :smile:

---
+++ eclipse_shared_workspace/DB_Test01/src/main.c
@@ -0,0 +1,62 @@
+/*
+ ============================================================================
+ Name        : DB_Test01.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+
+
+	FILE * fPointer = NULL;
+	//fPointer = fopen("C:/Users/DDevi/Desktop/Desk.txt","r"); // Test case if only files in appdata do not work
+	fPointer = fopen("C:/Users/DDevi/AppData/Local/GitHubDesktop/app-2.8.1/SoftwareEngineering/eclipse_shared_workspace/DB_Test01/Debug/Testdokument.txt","r");
+
+	/*Files in APP Data work too, yey
+	 *fopen returns 0 if file is not found
+	 * BE CAREFUL!!! replace "\" with "\\" or "/"
+	 * */
+
+	//stackflow code
+/*	char * buffer = 0;
+	long length;
+
+	if (fPointer)
+	{
+	  fseek (fPointer, 0, SEEK_END);
+	  length = ftell (fPointer);
+	  fseek (fPointer, 0, SEEK_SET);
+	  buffer = malloc (length);
+	  if (buffer)
+	  {
+	    fread (buffer, 1, length, fPointer);
+	  }
+	  fclose (fPointer);
+	}
+
+	if (buffer)
+	{
+	  // start to process your data / extract strings here...
+	}*/
+
+	char singleLine[150];
+
+	while(!feof(fPointer)){
+		fgets(singleLine, 150, fPointer);
+		puts(singleLine);
+	}
+
+	fclose(fPointer);
+
+	printf("Test success");
+
+
+	return 0;
+}
