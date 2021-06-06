055e9d702facbf37249a0c2306d76100cb95be73
DavidSch15 <84725190+DavidSch15@users.noreply.github.com>
2021-06-06 16:14:14 +0200

    .

--- eclipse_shared_workspace/Voktrainer_Final_Test/Debug/subdir.mk
+++ eclipse_shared_workspace/Voktrainer_Final_Test/Debug/subdir.mk
@@ -26,10 +26,10 @@ C_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-%.o: ../%.c subdir.mk
+%.o: ../%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cygwin C Compiler'
-	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
