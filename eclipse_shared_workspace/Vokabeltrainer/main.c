61b3622dce1a86a41d4524e953528aee3d9e11cd
Heino21 <hein_alex00@web.de>
2021-06-07 11:55:41 +0200

    Einfügen der Willkommensnachricht

--- eclipse_shared_workspace/Vokabeltrainer/main.c
+++ eclipse_shared_workspace/Vokabeltrainer/main.c
@@ -16,6 +16,12 @@ int main()
 
 	struct username *aktueller_nutzer = NULL;				//Erstelle pointer auf struct username um Rückgabewert der Funktion username_list verwenden zu können
 
+	printf("Willkommen zum Vokabeltrainer!\n\nVorab ein paar kurze Infos:\n"
+			"-Nach der Auswahl der abzufragenden Vokabeldatei, Abfragemodus und Anzahl der Vokabeln erfolgt die eigentliche Abfrage\n" //Begrüßung mit Ablauf des Programms
+			"-Du bekommst ein Feedback zum Festhalten deines Lernerfolgs\n"
+			"-Du kannst dein Koennen beweisen, indem du es in die Top3 der besten Vokabelpauker schaffst!\n"
+			"Und jetzt viel Spass!\n\n");
+
 	aktueller_nutzer = username_list();						//Funktion zeigt alle bestehenden Nutzer auf bzw. legt Usernames.txt Datei an, liest aktuellen Nutzernamen ein und returned Pointer auf den aktuellen Nutzer
 
 	printf("Willkommen in der Main %s", aktueller_nutzer->name);
