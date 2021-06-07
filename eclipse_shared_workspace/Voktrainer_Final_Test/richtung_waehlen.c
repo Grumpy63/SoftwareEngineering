8fb2e70c121fef6ffb2b250290e4ff8b586d667a
DavidSch15 <84725190+DavidSch15@users.noreply.github.com>
2021-06-07 16:16:20 +0200

    .

---
+++ eclipse_shared_workspace/Voktrainer_Final_Test/richtung_waehlen.c
@@ -0,0 +1,8 @@
+/*
+ * richtung_waehlen.c
+ *
+ *  Created on: 07.06.2021
+ *      Author: David
+ */
+
+
--- eclipse_shared_workspace/Voktrainer_Final_Test/vokabelzahl_feststellen.c
+++ eclipse_shared_workspace/Voktrainer_Final_Test/vokabelzahl_feststellen.c
@@ -13,12 +13,51 @@
 
 int vokabelzahl_feststellen(struct kategorie *abzufragende_kategorie){
 
-	printf("\nDie abzufragende Kategorie \"%s\" enthält %d Vokabeln.\n Wie viele möchten sie maximal abgefragt werden ?\n Maximale Anzahl Vokabeln:\n",abzufragende_kategorie->kategorie_name,abzufragende_kategorie->anzahl_in_kategorie);
-
+	int x = 0;
 	int anzahl = 0;
 
 	setbuf(stdout, NULL);
-	scanf("%d",anzahl);
+	printf("\nDie abzufragende Kategorie \"%s\" enthält %d Vokabeln.\n\n",abzufragende_kategorie->kategorie_name,abzufragende_kategorie->anzahl_in_kategorie);
+	printf("Möchten Sie über:\n");
+	printf("(1) alle Vokabeln abgefragt werden oder\n");
+	printf("(2) nur über eine bestimmte Anzahl abgefragt werden?\n");
+	while(1)
+	{
+		scanf("%d", &x);
+		if(x == 1)
+		{
+			return(abzufragende_kategorie->anzahl_in_kategorie);
+		}
+		else if(x == 2)
+		{
+			while(1)
+			{
+				printf("Wie viele Vokabeln möchten Sie abgefragt werden?:\n");
+				scanf("%d", &anzahl);
+				if(anzahl <= 0 || anzahl > abzufragende_kategorie->anzahl_in_kategorie)
+				{
+					printf("Die Eingabe ist ungültig.\n");
+
+				}
+				else
+				{
+					return(anzahl);
+				}
+			}
+
+		}
+		else{
+			printf("Die Eingabe ist ungültig bitte geben Sie Ihre Auswahl erneut ein.\n");
+
+		}
+
+	}
+
+
+
+
+
+
 
 	return anzahl;
 }
