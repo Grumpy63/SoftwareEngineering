30ee089309e5c50bf054f6e5e9bd6678a10ee48e
Heino21 <hein_alex00@web.de>
2021-06-08 15:17:22 +0200

    Update abfrage1.c

--- eclipse_shared_workspace/Vokabeltrainer/abfrage1.c
+++ eclipse_shared_workspace/Vokabeltrainer/abfrage1.c
@@ -12,23 +12,51 @@
 
 int abfrage1(struct kategorie* abzufragende_kategorie, int number_abzufragende_voc)
 {
-	int anzahl_korrekter_voc = 0;
-struct kategorie* help_ptr = NULL;
+
+int anzahl_korrekter_voc = 0;
+struct vokabel* help_ptr = NULL; 						//Hilfszeiger (Cursor)
 int zufall = 0;
+char dummy_zeichen = ' ';
+char eingabe [256];
 
-	for (int i=0; i<number_abzufragende_voc; i++)
+
+	for (int i=0; i<number_abzufragende_voc; i++)				//Schleife für die Abfragezyklen, i ist Laufvariable
 	{
-		//Zufallszahl erstellen
-		srand(time(NULL));
-		zufall = irand(0,abzufragende_kategorie->anzahl_in_kategorie);
-
-		for(int x=0; x<number_abzufragende_voc;x++) //pointer
 		{
+			srand(time(NULL));								//Generieren einer Zufallszahl von int in Bereich 0 bis x
+			zufall = irand(0,abzufragende_kategorie->anzahl_in_kategorie);
 
-		}
-		printf("Bitte geben sie die Übersetzung von %s ein:",abzufragende_kategorie->erste_vokabel->vokabel_sprache1);
-		scanf
-		//strcmp(...,...)
+
+			help_ptr = abzufragende_kategorie->erste_vokabel;			//Hilfszeiger auf Beginn der Vokabelliste
+
+
+			for(int x=0; x<zufall;x++) 				//Zeiger jeweils Vielfaches der Zufallszahl Weitersetzen auf die nächsten Listenelemente
+			{
+				help_ptr = help_ptr->next_vokabel;
+			}
+
+		} while(help_ptr->flag == 1)  //Prüfen ob Vokabel schon abgefragt wurde, wenn ja beginnt schleife von vorne mit neuer Zufallszahl
+
+		help_ptr->flag =1;  //Flag der Ausgewählten Vokabel setzen
+
+		printf("Bitte geben sie die Übersetzung von %s ein:",help_ptr->vokabel_sprache1);  // Aufforderung zur Eingabe
+		scanf("%[^\r\n]", eingabe);							//Es können auch Leerzeichen eingegeben werden
+		scanf("%c", &dummy_zeichen);										//Leeren des Puffers
+			if(dummy_zeichen == '\r')											//auch auf anderen OS (haben evtl mehr Zeichen)
+					{
+						scanf("%c", &dummy_zeichen);
+					}
+		if(strcmp(eingabe, help_ptr->vokabel_sprache2) == 0)  //Vergleich der beiden Strings. Groß/Kleinschreibung spielt noch eine Rolle!!!!
+			{
+				printf("Ihre Eingabe war richtig!");			//Ausgabe eines Feedbacks
+				anzahl_korrekter_voc++;							// Counter für richtige Vokabeln wird hochgesetzt
+			}
+		else
+			{
+				printf("Leider falsch! Korrekte wäre gewesen: %s", help_ptr->vokabel_sprache2);   //Feedback mit richtiger Übersetzung
+			}
+
+		help_ptr = NULL;	//Zeiger wird wieder geerdet
 	}
-return(0);
+return(anzahl_korrekter_voc);				//Rückgabe der Richtigen
 }
