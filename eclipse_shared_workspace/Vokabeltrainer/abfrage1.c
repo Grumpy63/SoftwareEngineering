70fce853924d6fca11f468354cac58b93bcb14de
Heino21 <hein_alex00@web.de>
2021-06-08 12:19:37 +0200

    Update abfrage1.c

--- eclipse_shared_workspace/Vokabeltrainer/abfrage1.c
+++ eclipse_shared_workspace/Vokabeltrainer/abfrage1.c
@@ -12,5 +12,23 @@
 
 int abfrage1(struct kategorie* abzufragende_kategorie, int number_abzufragende_voc)
 {
+	int anzahl_korrekter_voc = 0;
+struct kategorie* help_ptr = NULL;
+int zufall = 0;
+
+	for (int i=0; i<number_abzufragende_voc; i++)
+	{
+		//Zufallszahl erstellen
+		srand(time(NULL));
+		zufall = irand(0,abzufragende_kategorie->anzahl_in_kategorie);
+
+		for(int x=0; x<number_abzufragende_voc;x++) //pointer
+		{
+
+		}
+		printf("Bitte geben sie die Übersetzung von %s ein:",abzufragende_kategorie->erste_vokabel->vokabel_sprache1);
+		scanf
+		//strcmp(...,...)
+	}
 return(0);
 }
