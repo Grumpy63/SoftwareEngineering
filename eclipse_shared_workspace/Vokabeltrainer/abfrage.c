5def7a56a803d1fc721128eab351b840de931b22
Heino21 <hein_alex00@web.de>
2021-06-08 10:52:47 +0200

    Update 8.6. mit Krampus

--- eclipse_shared_workspace/Vokabeltrainer/abfrage.c
+++ eclipse_shared_workspace/Vokabeltrainer/abfrage.c
@@ -10,25 +10,21 @@
 #include <string.h>
 #include "voc_functions.h"								//Einbinden der Header-Dateien
 
-void abfrage(pointer, int number_abzufragende_voc, int number_voc_in_kategorie, int modus) //Definition der Abfrage-Fkt
-{
-
-	for(int i=0; i<number_abzufragende_voc; i++)
-	{
-
-		srand(time(NULL));										//Initialisieren des Seeds mit der Systemzeit
-		int irand( int a, int e)								//Erzeugen einer Zufallszahl vom Datentyp int im Intervall a-e
+//srand(time(NULL));										//Initialisieren des Seeds mit der Systemzeit, vor jedem Aufruf von irand verwenden!
+		int irand( int a, int e)								//Funktion zum Erzeugen von Zufallszahlen im Intervall a-e
 		{
 			double r = e - a + 1;
 			return a + (int)(r * rand()/(RAND_MAX+1.0));
 		}
 
+
+void abfrage(struct kategorie* abzufragende_kategorie, int number_abzufragende_voc, int modus) //Definition der Abfrage-Fkt
+{
 		switch (modus)											//Realisierung der verschiedenen Abfragerichtungen
 		{
-		case 1: break;
-		case 2: break;
-		case 3: break;
-		default: printf("Programminterner Fehler. Bitte starte die Anwendung erneut!");
+			case 1: break;										//Sprache1 -> Sprache2 in extra Funktionen seperat c files!
+			case 2: break;										//Sprache2 -> Sprache1
+			case 3: break;										//Gemischt per Zufall
+			default: printf("Programminterner Fehler. Bitte starte die Anwendung erneut!");
 		}
-	}
 }
---
+++ eclipse_shared_workspace/Vokabeltrainer/abfrage1.c
@@ -0,0 +1,8 @@
+/*
+ * abfrage1.c
+ *
+ *  Created on: 08.06.2021
+ *      Author: Alexander Hein
+ */
+
+
